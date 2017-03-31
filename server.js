93e3d7d9fb731d5ba50436171aca2a3238205a7e
jeantimex <jean.timex@gmail.com>
2017-03-31 15:19:42 -0700

    Implement the image upload server.

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var multer = require('multer');
+var path = require('path');
+var crypto = require('crypto');
+
+var storage = multer.diskStorage({
+  destination: './uploads',
+  filename: function(req, file, cb) {
+    return crypto.pseudoRandomBytes(16, function(err, raw) {
+      if (err) {
+        return cb(err);
+      }
+      return cb(null, '' + (raw.toString('hex')) + (path.extname(file.originalname)));
+    });
+  }
+});
+
+var app = new express();
+
+app.post('/', multer({
+  storage: storage
+}).single('upload'), function(req, res) {
+  console.log(req.file);
+  console.log(req.body);
+  return res.status(204).end();
+});
+
+app.listen(5000, console.log('Listening on port 5000'));
